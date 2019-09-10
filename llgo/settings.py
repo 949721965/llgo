5028d4f6048bc0a40c6020424f91e167cdf76014
zsimline <zsimline@163.com>
2019-09-10 19:29:01 +0800

    1928

--- llgo/settings.py
+++ llgo/settings.py
@@ -1,25 +1,25 @@
-# -*- coding: utf-8 -*-
-
-# Scrapy settings for llgo project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     https://doc.scrapy.org/en/latest/topics/settings.html
-#     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-#     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
-
 BOT_NAME = 'llgo'
 
 SPIDER_MODULES = ['llgo.spiders']
 NEWSPIDER_MODULE = 'llgo.spiders'
 
-
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'llgo (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
+
+
+# Sql Statement
+
+
+
+
+
+
+
+
+# UNUSED
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -88,3 +88,6 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+
+
